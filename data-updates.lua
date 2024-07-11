3275419eee41759fdc8588b0f8d25159d96b9994
Joe Taber / infogulch <infogulch@gmail.com>
2024-07-11 00:07:45 -0500

    Prefer factorio builtins

--- data-updates.lua
+++ data-updates.lua
@@ -22,9 +22,9 @@ do
       local localised_name = { "loaded-turrets.gun-turret-name", cap, { "item-name." .. ammo.name } }
       local capord = alphabet.sub(capidx, capidx + 1) .. "[" .. cap .. "]"
       local icons = {
-        merge({}, gunturreticon, { scale = .19, shift = { -1.2, -1.2 } }),
-        merge({}, ammoicon, { scale = .17, shift = { 1.2, 1.2 } }),
-        merge({}, ammoicon, { scale = .15, shift = { -1, 1.2 } }),
+        util.merge { gunturreticon, { scale = .19, shift = { -1.2, -1.2 } } },
+        util.merge { ammoicon, { scale = .17, shift = { 1.2, 1.2 } } },
+        util.merge { ammoicon, { scale = .15, shift = { -1, 1.2 } } },
       }
       if capidx == 1 then
         table.remove(icons, 3)
@@ -50,10 +50,10 @@ do
         ingredients = { { "gun-turret", 1 }, { "electronic-circuit", 1 }, { ammo.name, cap } },
         result = name,
       } --[[ @as data.RecipePrototype ]])
-      table.insert(entities, merge({}, gunturret, {
+      table.insert(entities, util.merge { gunturret, {
         name = name,
         localised_name = localised_name,
-      } --[[ @as data.EntityPrototype ]]))
+      } })
     end
   end
 end
@@ -65,7 +65,10 @@ do
   local ammo1icon = proto_icon(data.raw["ammo"]["firearm-magazine"])
   local ammo2icon = proto_icon(data.raw["ammo"]["piercing-rounds-magazine"])
 
-  local unlocks = map(recipes, function(r) return { type = "unlock-recipe", recipe = r.name } end)
+  local unlocks = {}
+  for _, r in pairs(recipes) do
+    table.insert(unlocks, { type = "unlock-recipe", recipe = r.name })
+  end
 
   technologies = {
     {
@@ -75,33 +78,32 @@ do
       effects = slice(unlocks, 1, 1),
       prerequisites = { "gun-turret", "military-2" },
       icons = {
-        merge({}, gttechicon, { scale = 0.19, shift = { -3.5, -3.5 } }),
-        merge({}, ammo1icon, { scale = .5, shift = { 7, 10 } }),
+        util.merge { gttechicon, { scale = 0.19, shift = { -3.5, -3.5 } } },
+        util.merge { ammo1icon, { scale = .5, shift = { 7, 10 } } },
       },
       order = "a-j-a",
     },
     {
       type = "technology",
       name = "loaded-turret-2",
-      unit = merge({}, mil3.unit, { count = mil3.unit.count / 2 }),
+      unit = util.merge { mil3.unit, { count = mil3.unit.count / 2 } },
       effects = slice(unlocks, 2),
       prerequisites = { "loaded-turret", "military-3" },
       icons = {
-        merge({}, gttechicon, { scale = 0.19, shift = { -3.5, -3.5 } }),
-        merge({}, ammo2icon, { scale = .5, shift = { 7, 10 } }),
-        merge({}, ammo2icon, { scale = .5, shift = { 0, 10 } }),
+        util.merge { gttechicon, { scale = 0.19, shift = { -3.5, -3.5 } } },
+        util.merge { ammo2icon, { scale = .5, shift = { 7, 10 } } },
+        util.merge { ammo2icon, { scale = .5, shift = { 0, 10 } } },
       },
       order = "a-j-a",
     },
   }
 end
 
-data:extend {
-  {
+data:extend(concat { items, recipes, entities, technologies,
+  { {
     type = "item-subgroup",
     name = "loaded-turret",
     group = "combat",
     order = "gg",
-  },
-  table.unpack(merge({}, items, recipes, entities, technologies)),
-}
+  } }
+})
--- tools.lua
+++ tools.lua
@@ -1,37 +1,3 @@
-function merge(...)
-    local tables_to_merge = { ... }
-    assert(#tables_to_merge > 1, "There should be at least two tables to merge them")
-
-    local result = tables_to_merge[1]
-
-    for i = 2, #tables_to_merge do
-        local from = tables_to_merge[i]
-        assert(type(from) == "table", string.format("Expected a table as function parameter %d", i))
-        for k, v in pairs(from) do
-            if type(k) == "number" then
-                table.insert(result, v)
-            elseif type(k) == "string" then
-                if type(v) == "table" then
-                    result[k] = result[k] or {}
-                    result[k] = merge(result[k], v)
-                else
-                    result[k] = v
-                end
-            end
-        end
-    end
-
-    return result
-end
-
-function map(list, f)
-    local result = {}
-    for _, v in ipairs(list) do
-        table.insert(result, f(v))
-    end
-    return result
-end
-
 function slice(list, i, j)
     local result = table.pack(table.unpack(list, i, j))
     result["n"] = nil -- :(
@@ -48,8 +14,12 @@ function proto_icon(proto --[[ @as data.IconData]])
     } --[[ @as data.IconData]]
 end
 
-return {
-    merge,
-    map,
-    slice,
-}
+function concat(arrays)
+    local result = {}
+    for _, arr in ipairs(arrays) do
+        for _, v in ipairs(arr) do
+            table.insert(result, v)
+        end
+    end
+    return result
+end
